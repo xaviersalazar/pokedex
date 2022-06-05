b1af9f3746cf4c7bfe2cd00f7c9c31d93dcb1f50
Xavier Salazar <xavierssalazar92@gmail.com>
2022-06-05 16:43:20 -0500

    adding basic styles; adding api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  NextUIProvider,
+  Text,
+  Loading,
+  Grid,
+  Card,
+  css,
+} from "@nextui-org/react";
+import Pokedex from "pokedex-promise-v2";
+
+const P = new Pokedex();
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    P.getResource(["/api/v2/pokemon"])
+      .then((res) => {
+        console.log(res);
+        const { count, next, previous, results } = res[0];
+
+        setPokemon({ count, next, previous, results });
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+      <Text h1 css={{ textAlign: "center" }}>
+        pokedex<span style={{ color: "#F31260" }}>.</span>
+      </Text>
+      {isLoading ? (
+        <Loading color="error" size="xl" />
+      ) : (
+        <Grid.Container gap={4} justify="center">
+          {console.log(pokemon)}
+          {pokemon?.results.map(({ name, url }) => (
+            <Grid xs={3}>
+              <Card
+                css={{
+                  minHeight: 250,
+                  marginTop: 8,
+                  marginBottom: 8,
+                }}
+              >
+                <Text h6>{name}</Text>
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
+    </NextUIProvider>
   );
 }
 
